c0950f1db856b9913ceec1c8eef71606f1f11c42
yoppei0818 <yoppei0818@gmail.com>
2025-03-26 17:41:50 +0900

    #1 環境構築

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
